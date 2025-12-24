9f85ecd61bc896fa65fa4342cd2b4c40037ecbf0
lolofficial <lolgamer12_yt@proton.me>
2025-12-24 10:23:37 +0100

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,3 @@
-from fastapi import FastAPI, APIRouter, HTTPException, Request, Depends from fastapi.responses 
-import StreamingResponse from starlette.middleware.cors import CORSMiddleware from pydantic 
-import BaseModel, EmailStr from typing import List, Dict, Optional, AsyncGenerator from datetime
-import datetime, timedelta from jose import jwt, JWTError import os, uuid, asyncio, json
+rom fastapi import FastAPI, APIRouter, HTTPException, Path, Body, Request, Depends from fastapi.responses import StreamingResponse, JSONResponse from starlette.middleware.cors import CORSMiddleware from motor.motor_asyncio import AsyncIOMotorClient from pydantic import BaseModel, Field, EmailStr from typing import List, Optional, Literal, AsyncGenerator from datetime import datetime, timedelta from jose import jwt, JWTError from passlib.hash import bcrypt import os, uuid, asyncio, json, logging, requests
 
-app = FastAPI() api = APIRouter(prefix="/api")
+
